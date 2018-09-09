d1543b76570e391af6a0443001024841b500e177
QMaximillian <quinnlashinsky@gmail.com>
2018-09-09 18:13:54 -0400

    Closer

--- app/serializers/music_message_serializer.rb
+++ app/serializers/music_message_serializer.rb
@@ -1,5 +1,7 @@
 class MusicMessageSerializer < ActiveModel::Serializer
   attributes :id, :music_channel_id, :content
-  belongs_to :music_channel
+  # belongs_to :music_channel,
+
+  serializer: MusicMessageSerializer, include_nested_associations: true
 
 end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,10 +1,10 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :username
+  attributes :id, :username, :music_channels, :music_messages
 
-  has_many :music_channels
-  has_many :music_messages, through: :music_channels
-  # serializer: MusicMessageSerializer,
-  # include_nested_associations: true
+  # has_many :music_channels
+  # has_many :music_messages, through: :music_channels
+
+  # serializer: MusicChannelSerializer, include_nested_associations: true
   # #
   # has_many :music_channels, serializer: MusicChannelSerializer
   # serializer: LyricChannelSerializer
